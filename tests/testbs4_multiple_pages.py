bc4b05922120b3a54cfcd7e916750dc7d95691cc
danielrobl3s <danielrobles200217@gmail.com>
2024-02-12 23:11:47 -0600

    multiple pages with beautiful soup

--- tests/testbs4_multiple_pages.py
+++ tests/testbs4_multiple_pages.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 
-website = 'https://subslikescript.com/movie/Titanic-120338'
+website = 'https://subslikescript.com/movies'
 result = requests.get(website)
 content = result.text
 
@@ -10,10 +10,17 @@ soup = BeautifulSoup(content, 'lxml')
 
 box = soup.find('article', class_ ='main-article')
 
-title = box.find('h1').get_text()
+links = []
+for link in box.find_all('a', href=True):
+    links.append(link['href'])
+
+
+print(links)
+
+""" title = box.find('h1').get_text()
 
 fullScript = box.find('div', class_ = 'full-script').get_text(strip=True, separator=' ')
 
 with open(f'{title}.txt', 'w', encoding='utf-8') as file:
-    file.write(fullScript)
+    file.write(fullScript) """
     
