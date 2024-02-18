e3deceb1df283e250dae5ff95b011b100cd94c60
danielrobl3s <danielrobles200217@gmail.com>
2024-02-17 23:46:40 -0600

    ask for credentials before loging in

--- tests/facebook_Test.py
+++ tests/facebook_Test.py
@@ -3,6 +3,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
+username = input('Tell me your email: ')
+password = input('Tell me your password: ')
+
 #Create the web driver to GET request this facebook page:
 website = 'https://www.facebook.com/POSTADurango'
 driver = webdriver.Chrome()
@@ -14,8 +17,8 @@ driver.implicitly_wait(60) #wait 60 seconds or until everything is found
 
 try:
     #Get email and password from the facebook login popup to later send them
-    email = driver.find_element(By.XPATH, '//input[@class="x1i10hfl xggy1nq x1s07b3s x1kdt53j x1a2a7pz xjbqb8w x76ihet xwmqs3e x112ta8 xxxdfa6 x9f619 xzsf02u x1uxerd5 x1fcty0u x132q4wb x1a8lsjc x1pi30zi x1swvt13 x9desvi xh8yej3 x15h3p50 x10emqs4"]').send_keys("dudedeveloper08@gmail.com")
-    pw = driver.find_element(By.XPATH, '//input[(@aria-invalid="false") and (@name="pass")]').send_keys("Este es el correo del dude developer 89")
+    email = driver.find_element(By.XPATH, '//input[@class="x1i10hfl xggy1nq x1s07b3s x1kdt53j x1a2a7pz xjbqb8w x76ihet xwmqs3e x112ta8 xxxdfa6 x9f619 xzsf02u x1uxerd5 x1fcty0u x132q4wb x1a8lsjc x1pi30zi x1swvt13 x9desvi xh8yej3 x15h3p50 x10emqs4"]').send_keys(username)
+    pw = driver.find_element(By.XPATH, '//input[(@aria-invalid="false") and (@name="pass")]').send_keys(password)
     login = driver.find_element(By.XPATH, '//div[@class="x1c436fg"]/div[(@aria-label="Accessible login button") and (@role="button")]')
     login.click()
 
