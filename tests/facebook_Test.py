21632577f7824a5adf98f9450b99390876956bcc
danielrobl3s <danielrobles200217@gmail.com>
2024-02-17 18:18:32 -0600

    introducing email and password in the fields

--- tests/facebook_Test.py
+++ tests/facebook_Test.py
@@ -13,14 +13,13 @@ driver.get(website)
 driver.implicitly_wait(60) #wait 60 seconds or until everything is found
 
 #Get email and password from the facebook login popup to later send them
-try:
-    email = driver.find_element(By.XPATH, '//input[@class="x1i10hfl xggy1nq x1s07b3s x1kdt53j x1a2a7pz xjbqb8w x76ihet xwmqs3e x112ta8 xxxdfa6 x9f619 xzsf02u x1uxerd5 x1fcty0u x132q4wb x1a8lsjc x1pi30zi x1swvt13 x9desvi xh8yej3 x15h3p50 x10emqs4"]').send_keys("dudedeveloper08@gmail.com")
-    pw = driver.find_element(By.XPATH, '//input[@type="password"]')
-    login = driver.find_element(By.XPATH, '//div[@class="x1n2onr6 x1ja2u2z x78zum5 x2lah0s xl56j7k x6s0dn4 xozqiw3 x1q0g3np xi112ho x17zwfj4 x585lrc x1403ito x972fbf xcfux6l x1qhh985 xm0m39n x9f619 xn6708d x1ye3gou xtvsq51 x1fq8qgq"]').send_keys("Este es el correo del dude developer 89")
-    login.click()
-    time.sleep(5)
-except:
-    print("Algo salio mal, revisa que los elementos si sean interactivos")
+email = driver.find_element(By.XPATH, '//input[@class="x1i10hfl xggy1nq x1s07b3s x1kdt53j x1a2a7pz xjbqb8w x76ihet xwmqs3e x112ta8 xxxdfa6 x9f619 xzsf02u x1uxerd5 x1fcty0u x132q4wb x1a8lsjc x1pi30zi x1swvt13 x9desvi xh8yej3 x15h3p50 x10emqs4"]').send_keys("dudedeveloper08@gmail.com")
+pw = driver.find_element(By.XPATH, '//input[(@aria-invalid="false") and (@name="pass")]').send_keys("Este es el correo del dude developer 89")
+login = driver.find_element(By.XPATH, '//input[(@class="x1i10hfl xggy1nq x1s07b3s x1kdt53j x1a2a7pz xjbqb8w x1n2xptk xkbpzyx xdppsyt x1rr5fae xhk9q7s x1otrzb0 x1i1ezom x1o6z2jb x9f619 xzsf02u x1qlqyl8 xk50ysn x1vqgdyp x1y1aw1k xn6708d x1a8lsjc x1ye3gou x1yc453h x35muul xha3pab x10lcxz4 xzt8jt4 xiighnt xviufn9 x1umwzx8 xw6yb2j x7c2ntu x8tb6ha x1irh2kb") and (@name="pass")]').send_keys("Este es el correo del dude developer 89")
+login.click()
+time.sleep(5)
+
+    
 
 print(driver.current_url)
 
