39980e69d27c76bf02410a8f51b04b5d0a88e19a
danielrobl3s <danielrobles200217@gmail.com>
2024-02-24 13:37:43 -0600

    Instagram scraper done

--- tests/instagram_Test.py
+++ tests/instagram_Test.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
+import csv
 import time
 
 username = 'dudedeveloper08@gmail.com'
@@ -46,8 +47,16 @@ for link in links:
     total_titles.append(title)
     like = driver.find_element(By.XPATH, '//div[@class="x1i10hfl xjbqb8w x1ejq31n xd10rxx x1sy0etr x17r0tee x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz"]//span[@class="html-span xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x1hl2dhg x16tdsg8 x1vvkbs"]').text
     total_likes.append(like)
+    comment = driver.find_element(By.XPATH, '//div[@class="x9f619 xjbqb8w x78zum5 x168nmei x13lgxp2 x5pf9jr xo71vjh x1xmf6yo x1uhb9sk x1plvlek xryxfnj x1c4vz4f x2lah0s xdt5ytf xqjyukv x1qjc9v5 x1oa3qoh x1nhvcw1"]/span[@class="x1lliihq x1plvlek xryxfnj x1n2onr6 x193iq5w xeuugli x1fj9vlw x13faqbe x1vvkbs x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x x1i0vuye x1fhwpqd xo1l8bm x5n08af x1s3etm8 x676frb x10wh9bi x1wdrske x8viiok x18hxmgj"]').text
+    comments.append(comment)
     go_back = driver.find_element(By.XPATH, '//div[@class="x10l6tqk x1nc75zl xi3dyvs x1mywscw"]/div')
     go_back.click()
     time.sleep(3)
 
-print(total_titles, total_likes)
+with open('postsIG.csv', 'w', encoding='utf-8') as file:
+    fieldnames = ['Title', 'Likes', 'Comments']
+    writer = csv.DictWriter(file, fieldnames=fieldnames)
+    writer.writeheader()
+
+    for data in zip(total_titles, total_likes, comments):
+        writer.writerow({"Title": data[0], "Likes": data[1], "Comments": data[2]})
