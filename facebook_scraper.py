ad7fc727ca5e2b512f7f547a483210c827ea93cf
danielrobl3s <danielrobles200217@gmail.com>
2024-02-25 19:16:28 -0600

    formato de scraper corregido, completado

--- facebook_scraper.py
+++ facebook_scraper.py
@@ -42,23 +42,21 @@ reactions = [element.text for element in reacts]
 
 comments_and_shares = driver.find_elements(By.XPATH, '//div[@class="x6s0dn4 xi81zsa x78zum5 x6prxxf x13a6bvl xvq8zen xdj266r xktsk01 xat24cr x1d52u69 x889kno x4uap5 x1a8lsjc xkhd6sd xdppsyt"]//div[@class="x1i10hfl x1qjc9v5 xjqpnuy xa49m3k xqeqjp1 x2hbi6w x1ypdohk xdl72j9 x2lah0s xe8uvvx x2lwn1j xeuugli x1hl2dhg xggy1nq x1t137rt x1o1ewxj x3x9cwd x1e5q0jg x13rtm0m x3nfvp2 x1q0g3np x87ps6o x1a2a7pz xjyslct xjbqb8w x13fuv20 xu3j5b3 x1q0q8m5 x26u7qi x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1heor9g xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x1n2onr6 x16tdsg8 x1ja2u2z xt0b8zv"]')
 comments_shares = [element.text for element in comments_and_shares]
-shares = comments_shares[::2]
+shares = []
+comments = comments_shares[::2]
 
-print(comments_shares, shares)
-
-""" coments = driver.find_elements(By.XPATH, '//div[@class="x6s0dn4 xi81zsa x78zum5 x6prxxf x13a6bvl xvq8zen xdj266r xktsk01 xat24cr x1d52u69 x889kno x4uap5 x1a8lsjc xkhd6sd xdppsyt"]//div[@id=":r1nc:"]//span')
-comments = [element.text for element in coments] """
-
-""" share = driver.find_elements(By.XPATH, '(/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[4]/div[2]/div/div[2]/div[3]/div[1]/div/div/div/div/div/div/div/div/div/div/div[2]/div/div/div[4]/div/div/div[1]/div/div[1]/div/div[2]/div[3]/span/div/div/div[1]/span')
-shares = [element.text for element in share] """
-
+index = 0
+for comment in comments_and_shares:    
+    if index%2 != 0:
+        shares.append(comment.text)
+    index += 1
 
 with open(f'{name}.csv', 'w', newline='', encoding='utf-8') as csvfile:
-    fieldnames = ['title', 'reactions', 'comments']
+    fieldnames = ['title', 'reactions', 'comments', 'shares']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
     
 
-    for data in zip(titles, reactions):
-        writer.writerow({"title": data[0], "reactions": data[1]})
+    for data in zip(titles, reactions, comments, shares):
+        writer.writerow({"title": data[0], "reactions": data[1], "comments": data[2], "shares": data[3]})
     
