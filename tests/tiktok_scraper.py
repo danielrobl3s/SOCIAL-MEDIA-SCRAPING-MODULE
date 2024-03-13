8a59909a2399f29e02f00cf11a0bb22c7b92ebdf
danielrobl3s <danielrobles200217@gmail.com>
2024-03-13 13:37:58 -0600

    tiktok scraper file created

---
+++ tests/tiktok_scraper.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.actions.wheel_input import ScrollOrigin
+from bs4 import BeautifulSoup
+import urllib3
+import csv
+import re
+import time
+
