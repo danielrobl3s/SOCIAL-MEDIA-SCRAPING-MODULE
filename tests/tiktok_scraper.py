892876734182f21be8bc738372affb469dbb1dea
danielrobl3s <danielrobles200217@gmail.com>
2024-03-13 15:22:01 -0600

    Tiktok scraper done

--- tests/tiktok_scraper.py
+++ tests/tiktok_scraper.py
@@ -13,9 +13,12 @@ username = "dudedeveloper08@gmail.com"
 password = "Obaboyamama@12"
 prefix = "https://www.tiktok.com/@"
 titles = []
+likes = []
+comments = []
+saved = []
 
 website = prefix + input('Introduce la cuenta de tiktok a la que quieres acceder: ')
-#name = input('Nombra tu archivo .csv: ')
+name = input('Nombra tu archivo .csv: ')
 
 chrome_options = webdriver.ChromeOptions()
 prefs = {"profile.default_content_setting_values.notifications" : 2}
@@ -49,11 +52,27 @@ posts = driver.find_elements(By.XPATH, '//div[@class="css-x6y88p-DivItemContaine
 
 for post in posts:
     post.click()
+
     title = driver.find_element(By.XPATH, '//div[@class="css-1nst91u-DivMainContent e1mecfx01"]').text
     titles.append(title)
+
+    like = driver.find_element(By.XPATH, '//strong[@data-e2e="browse-like-count"]').text
+    likes.append(like)
+
+    comments_number = driver.find_element(By.XPATH, '//strong[@data-e2e="browse-comment-count"]').text
+    comments.append(comments_number)
+
+    saved_number = driver.find_element(By.XPATH, '//strong[@data-e2e="undefined-count"]').text
+    saved.append(saved_number)
 
     close = driver.find_element(By.XPATH, '//button[@aria-label="Close"]')
     close.click()
 
 
-print(titles)
+with open(f'{name}.csv', 'w', encoding='utf-8') as file:
+    fieldnames = ['Title', 'Likes', 'Comments', 'Saved']
+    writer = csv.DictWriter(file, fieldnames=fieldnames)
+    writer.writeheader()
+
+    for data in zip(titles, likes, comments, saved):
+        writer.writerow({"Title": data[0], "Likes": data[1], "Comments": data[2], "Saved": data[3]})
