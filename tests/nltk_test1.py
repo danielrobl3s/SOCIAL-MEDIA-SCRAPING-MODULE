8c7e24b4256eec4e8063bd3bc99402d7d325e988
danielrobl3s <danielrobles200217@gmail.com>
2024-03-28 16:05:47 -0600

    nlp: word-tokenizying function

--- tests/nltk_test1.py
+++ tests/nltk_test1.py
@@ -7,6 +7,15 @@ from nltk.stem import WordNetLemmatizer
 from nltk import FreqDist
 import csv
 
+#Function to tokenize titles
+def turn_tokens(titles):
+    title_tokens = []
+    for title in titles:
+        new_list_title = word_tokenize(title)
+        title_tokens += new_list_title
+    return title_tokens
+
+
 #Get file and read it with pandas
 your_file = input('Introduce el nombre del archivo: ')
 df = pd.read_csv(your_file)
@@ -14,7 +23,8 @@ df = pd.read_csv(your_file)
 #Get just the titles to apply nlp
 titles = df.iloc[:,0]
 
+#Tokenize every title
+tokens = turn_tokens(titles)
 
-
-
-
+print(str(len(titles)))
+print(tokens)
