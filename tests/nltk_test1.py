6dccc2db810d9fd8589723dc720ea76b9b576d83
danielrobl3s <danielrobles200217@gmail.com>
2024-03-27 23:01:20 -0600

    nlp: get csv file and get titles to apply nlp

--- tests/nltk_test1.py
+++ tests/nltk_test1.py
@@ -7,18 +7,14 @@ from nltk.stem import WordNetLemmatizer
 from nltk import FreqDist
 import csv
 
-text = ''
+#Get file and read it with pandas
+your_file = input('Introduce el nombre del archivo: ')
+df = pd.read_csv(your_file)
 
-with open('canal10posts - canal10posts.csv.csv', 'r', encoding='utf-8') as f:
-    text = f.read()
+#Get just the titles to apply nlp
+titles = df.iloc[:,0]
 
 
-tokens = word_tokenize(text.lower())
-text1 = nltk.Text(tokens)
 
-#print(text1.similar("PANORAMA"))
 
-frdist = FreqDist([word for word in set(text1)])
-
-print(frdist.items())
 
