c919b0eae4c68bb8dc03039e884aa1bb9604ced5
danielrobl3s <danielrobles200217@gmail.com>
2024-04-01 23:01:17 -0600

    modified nlp: titles_length feature and most_common_words added

--- tests/nltk_test1.py
+++ tests/nltk_test1.py
@@ -1,4 +1,5 @@
 import nltk
+nltk.download('stopwords')
 import pandas as pd
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from nltk.corpus import stopwords
@@ -10,15 +11,26 @@ import csv
 
 #Function to tokenize titles
 def turn_tokens(titles):
+    titles_length = []
     title_tokens = []
     most_common = []
+    new_list_title_no_stop_words = []
+
     for title in titles:
         new_list_title = word_tokenize(title)
-        fd = FreqDist(new_list_title)
+        titles_length.append(len(new_list_title))
+
+        for word in new_list_title:
+
+            if word not in stop_words:
+                new_list_title_no_stop_words.append(word)
+
+
+        fd = FreqDist(new_list_title_no_stop_words)
         most_common.append(fd.most_common(3))
-        title_tokens.append(new_list_title)
+        title_tokens.append(new_list_title_no_stop_words)
         
-    return title_tokens, 'MOST COMMON NEXT ----------------------------->', most_common
+    return title_tokens, titles_length, 'MOST COMMON NEXT ----------------------------->', most_common
 
 
 #Get file and read it with pandas
@@ -28,8 +40,15 @@ df = pd.read_csv(your_file)
 #Get just the titles to apply nlp
 titles = df.iloc[:,0]
 
+#Stopwords:
+stop_words = stopwords.words('Spanish')
+stop_words.append('.')
+
 #Tokenize every title
 tokens = turn_tokens(titles)
 
 print(str(len(titles)))
 print(tokens)
+
+
+
