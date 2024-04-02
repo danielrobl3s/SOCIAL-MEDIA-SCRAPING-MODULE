b278c197961d9ffdd2651429987d8db47ed2be0d
danielrobl3s <danielrobles200217@gmail.com>
2024-04-01 21:48:54 -0600

    modified nlp: returns tokenized titles as array each and the 3 most common words of each title

--- tests/nltk_test1.py
+++ tests/nltk_test1.py
@@ -3,6 +3,7 @@ import pandas as pd
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from nltk.probability import FreqDist
 from nltk.stem import WordNetLemmatizer
 from nltk import FreqDist
 import csv
@@ -10,10 +11,14 @@ import csv
 #Function to tokenize titles
 def turn_tokens(titles):
     title_tokens = []
+    most_common = []
     for title in titles:
         new_list_title = word_tokenize(title)
-        title_tokens += new_list_title
-    return title_tokens
+        fd = FreqDist(new_list_title)
+        most_common.append(fd.most_common(3))
+        title_tokens.append(new_list_title)
+        
+    return title_tokens, 'MOST COMMON NEXT ----------------------------->', most_common
 
 
 #Get file and read it with pandas
